d0c641cd01f08703c44f0720e903bda14dc8207e
mijeong <mijeong.park@etri.re.kr>
2025-08-13 15:14:59 +0900

    o. updated

---
+++ platform/test_prj_mj/imp_genesys2_2025-08-08/include/ervp_design_info.vh
@@ -0,0 +1,5 @@
+wire [BW_USERNAME-1:0] username_string = `FORMAT_STRING("None");
+wire [BW_GIT_NAME-1:0] home_git_name_string = `FORMAT_STRING("rvx_tutorial_mj2");
+wire [BW_GIT_VERSION-1:0] home_git_version_string = `FORMAT_STRING("cfc87d6");
+wire [BW_GIT_VERSION-1:0] devkit_git_version_string = `FORMAT_STRING("fc2cb49");
+wire [BW_DATE-1:0] design_date_string = `FORMAT_STRING("2025-08-08 11:21");
---
+++ platform/test_prj_mj/imp_genesys2_2025-08-08/xci/xilinx_clock_pll_0/xilinx_clock_pll_0_stub.v
@@ -0,0 +1,21 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
+// Date        : Fri Aug  8 11:22:20 2025
+// Host        : mj-940XGK running 64-bit Ubuntu 24.04.1 LTS
+// Command     : write_verilog -force -mode synth_stub
+//               /home/mj/rvx_tutorial_mj2/platform/test_prj_mj/imp_genesys2_2025-08-08/xci/xilinx_clock_pll_0/xilinx_clock_pll_0_stub.v
+// Design      : xilinx_clock_pll_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7k325tffg900-2
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module xilinx_clock_pll_0(clk_50000000, clk_in1_p, clk_in1_n)
+/* synthesis syn_black_box black_box_pad_pin="clk_50000000,clk_in1_p,clk_in1_n" */;
+  output clk_50000000;
+  input clk_in1_p;
+  input clk_in1_n;
+endmodule
